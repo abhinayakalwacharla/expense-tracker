957cf052f897be4a8f2ac4fbbabfa5efaaadfbc0
abhinayakalwacharla173@gmail.com <abhinayakalwacharla173@gmail.com>
2025-11-01 00:59:22 +0530

    Auto-commit: Expense.js updated

--- backend/models/Expense.js
+++ backend/models/Expense.js
@@ -1,10 +1,9 @@
 import mongoose from "mongoose";
 
 const expenseSchema = new mongoose.Schema({
-title: { type: String, required: true },
-amount: { type: Number, required: true },
-category: { type: String, required: true },
-date: { type: Date, default: Date.now }
+    title: { type: String, required: true },
+    amount: { type: Number, required: true },
 });
 
-export default mongoose.model("Expense", expenseSchema);
+const Expense = mongoose.model("Expense", expenseSchema);
+export default Expense;
