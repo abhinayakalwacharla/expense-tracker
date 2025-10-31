67a5f3d90bdafa4498110c8b9572b932350234a7
abhinayakalwacharla173@gmail.com <abhinayakalwacharla173@gmail.com>
2025-11-01 00:31:23 +0530

    Auto-commit: api.js updated

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,13 +1,9 @@
 import axios from "axios";
 
-const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000"
+const API = axios.create({
+  baseURL: "http://localhost:5000/api", // backend URL
 });
 
-api.interceptors.request.use(config => {
-    const token = localStorage.getItem("token");
-    if (token) config.headers.Authorization = `Bearer ${token}`;
-    return config;
-});
-
-export default api;
+export const getExpenses = () => API.get("/expenses");
+export const addExpense = (expenseData) => API.post("/expenses", expenseData);
+export const deleteExpense = (id) => API.delete(`/expenses/${id}`);
