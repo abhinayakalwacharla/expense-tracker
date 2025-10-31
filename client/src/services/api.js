e3ce600a590b7a358d17bca504d1df7586f1017d
abhinayakalwacharla173@gmail.com <abhinayakalwacharla173@gmail.com>
2025-11-01 01:00:50 +0530

    Auto-commit: api.js updated

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // Make sure backend runs on port 5000
+    baseURL: "http://localhost:5000/api",
 });
 
-export default api;
+export const getExpenses = () => api.get("/");
+export const addExpense = (data) => api.post("/", data);
+export const deleteExpense = (id) => api.delete(`/${id}`);
